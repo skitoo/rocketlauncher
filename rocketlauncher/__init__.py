b0a281cb069f56cf68ff51c37b8a8bd1af9a8ebd
Alexis Couronne <alexis@stormz.me>
2018-02-21 13:57:46 +0100

    Add rocketlauncher package

---
+++ rocketlauncher/__init__.py
@@ -0,0 +1,15 @@
+import sys
+
+if sys.version_info[0] < 3:
+    sys.exit('Python < 3 is unsupported')
+
+
+__title__ = 'rocketlauncher'
+__description__ = 'A Recalbox frontend'
+__url__ = 'https://github.com/skitoo/rocketlauncher'
+__version__ = '0.1.0'
+__build__ = 0x000100
+__author__ = 'Alexis Couronne'
+__author_email__ = 'rocket@skitoo.net'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018 Alexis Couronne'
