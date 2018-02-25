4561e7936632614bbd22cfe09157672ca152edec
Alexis Couronne <alexis@skitoo.net>
2018-02-25 21:43:09 +0100

    Add properties to Rocket to access to files path

--- rocketlauncher/database.py
+++ rocketlauncher/database.py
@@ -1,10 +1,15 @@
+import os
+import glob
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, UniqueConstraint, Integer, String, Date, PickleType
+
 
 Base = declarative_base()
 
 
 class Rocket(Base):
+    BASE_PATH = ''
+
     __tablename__ = 'rocket'
     __table__args = (
         UniqueConstraint('name', 'platform', name='unique_on_name_and_platform'),
@@ -20,3 +25,31 @@ class Rocket(Base):
     publisher = Column(String, nullable=False)
     players = Column(PickleType, nullable=False)
     releasedate = Column(Date, nullable=False)
+
+    def __str__(self):
+        return '<Rocket %s::%s>' % (self.platform, self.name)
+
+    @property
+    def path(self):
+        print(Rocket.BASE_PATH)
+        return os.path.join(Rocket.BASE_PATH, self.platform, self.name)
+
+    @property
+    def rom_path(self):
+        return os.path.join(self.path, self.rom)
+
+    @property
+    def logo_path(self):
+        return os.path.join(self.path, 'logo.png')
+
+    @property
+    def front_path(self):
+        return os.path.join(self.path, 'front.jpg')
+
+    @property
+    def videos_path(self):
+        return glob.glob(os.path.join(self.path, 'video*.gif'))
+
+    @property
+    def screenshots_path(self):
+        return glob.glob(os.path.join(self.path, 'screenshot*.jpg'))
